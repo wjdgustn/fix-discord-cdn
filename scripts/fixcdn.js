1c65c61e3ab4bd0e7ab499796d7115c751158e75
HYONSU <admin@hyonsu.com>
2024-07-05 15:59:05 +0900

    Firefox 지원

--- scripts/fixcdn.js
+++ scripts/fixcdn.js
@@ -1,8 +1,18 @@
+const fixcdn = () => location.href = 'https://fixcdn.hyonsu.com' + location.pathname;
+
 (() => {
+    const url = new URL(location.href);
+
+    const exStr = url.searchParams.get('ex');
+    const exTime = parseInt(exStr, 16) * 1000;
+    if(isNaN(exTime) || exTime <= Date.now()) return fixcdn();
+
     const navigationData = performance.getEntriesByType('navigation')[0];
     const responseCode = navigationData.responseStatus;
 
-    if(responseCode !== 404) return;
+    if(responseCode === 404) return fixcdn();
 
-    location.href = 'https://fixcdn.hyonsu.com' + location.pathname;
+    document.addEventListener('DOMContentLoaded', () => {
+        if(document.body.innerText.trim() === 'This content is no longer available.') return fixcdn();
+    });
 })()
